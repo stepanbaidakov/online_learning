96400303e10c9e0f23c12da5ca98321098166a89
Stepan Baidakov <stepanbaidakov@icloud.com>
2026-05-17 17:07:41 +0200

    first commit

---
+++ users/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from .managers import CustomUserManager
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=35, verbose_name="Ф.И.О.", null=True, blank=True)
+    avatar = models.ImageField(upload_to="avatars", blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        permissions = [("can_block_user", "Can block user")]
