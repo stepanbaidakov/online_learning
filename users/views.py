61789cf8ad4c563b8a0e179d0bed98ee84b02cc8
Stepan Baidakov <stepanbaidakov@icloud.com>
2026-05-19 18:22:19 +0200

    small fixes in users/views.py

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
+from .serializers import UserSerializer
 from .models import CustomUser
 
 # Create your views here.
 
 class UserUpdateAPIView(generics.UpdateAPIView):
-    serializer_class = CustomUser
+    serializer_class = UserSerializer
     queryset = CustomUser.objects.all()
