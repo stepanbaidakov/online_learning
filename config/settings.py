34c9d260154b3c70432d5ce9b82cc89f9dda3612
Stepan Baidakov <stepanbaidakov@icloud.com>
2026-06-14 14:03:17 +0200

    homework 32.2 improved

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'materials',
     'drf_yasg',
     'corsheaders',
+    'django_celery_beat'
 ]
 
 MIDDLEWARE = [
@@ -170,3 +171,17 @@ CACHES = {
         'LOCATION': os.getenv("LOCATION"),
     }
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379'  # Например, Redis, который по умолчанию работает на порту 6379
+
+# URL-адрес брокера результатов, также Redis
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+# Часовой пояс для работы Celery
+CELERY_TIMEZONE = "Europe/Zurich"
+
+# Флаг отслеживания выполнения задач
+CELERY_TASK_TRACK_STARTED = True
+
+# Максимальное время на выполнение задачи
+CELERY_TASK_TIME_LIMIT = 30 * 60
--- users/views.py
+++ users/views.py
@@ -11,6 +11,7 @@ import os
 from stripe import StripeClient
 from materials.models import Course
 from django.urls import reverse_lazy
+from .services import create_checkout_session
 
 # Create your views here.
 
@@ -52,34 +53,8 @@ class PaymentListAPIView(generics.ListAPIView):
 
 class CreatePaymentAPIView(APIView):
     def get(self, request, *args, **kwargs):
-        course = Course.objects.get(pk=kwargs.get("pk"))
-        api_key = os.getenv("API_KEY")
-        client = StripeClient(api_key)
-
-        product = client.v1.products.create({
-            "name": course.title
-        })
-        price = client.v1.prices.create({
-            "product": product.id,
-            "unit_amount": int(course.price * 100),
-            "currency": "usd"
-        })
-
-        session = client.v1.checkout.sessions.create({
-            "success_url": f"http://localhost:8000/courses/{course.id}/",
-            "line_items": [{"price": price.id, "quantity": 1}],
-            "mode": "payment",
-        })
-        Payment.objects.create(
-            user=request.user,
-            course=course,
-            amount=session.amount_total,
-            status="unpaid",
-            stripe_session_id=session.id,
-        )
-
-        payment_status = reverse_lazy("users:payment-status", kwargs={"session_id": session.id})
-        return Response({"checkout_url": session.url, "payment_status_url": payment_status})
+        result = create_checkout_session(request, *args, **kwargs)
+        return result
 
 
 class PaymentStatusAPIView(APIView):
